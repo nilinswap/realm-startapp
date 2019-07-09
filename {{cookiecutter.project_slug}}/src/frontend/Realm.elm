cb41c832082cb382f7ab02dbc170a86164ec76db
nilinswap <nilinswap@gmail.com>
2019-07-09 12:05:38 +0530

    realm minor

--- {{cookiecutter.project_slug}}/src/frontend/Realm.elm
+++ {{cookiecutter.project_slug}}/src/frontend/Realm.elm
@@ -1,23 +1,27 @@
-port module Realm exposing (..)
-import Json.Encode as JE
+port module Realm exposing (Flag, WidgetSpec, child, loadWidget, wrapped)
+
 import Html as H exposing (..)
 import Html.Attributes as H exposing (..)
+import Json.Encode as JE
+
 
 port loadWidget : JE.Value -> Cmd msg
 
-type alias WidgetSpec = {
-    id: String
-    , config: JE.Value
-    ,uid: String
 
-  }
-
-type alias Flag config = {
-        uid: String,
-        config: config
+type alias WidgetSpec =
+    { id : String
+    , config : JE.Value
+    , uid : String
     }
 
-child : WidgetSpec ->  Html msg
+
+type alias Flag config =
+    { uid : String
+    , config : config
+    }
+
+
+child : WidgetSpec -> Html msg
 child spec =
     H.div [ H.id spec.uid ] []
 
@@ -25,5 +29,3 @@ child spec =
 wrapped : String -> List (Html msg) -> Html msg
 wrapped uid html =
     H.div [ H.id uid ] html
-
-
